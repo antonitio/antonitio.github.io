815f3b6784a4bd7ddb54de7674c0834c5a335b60
a7o <a7o@gmail.com>
2020-01-09 16:25:50 +1100

    commit

--- top.js
+++ top.js
@@ -4,6 +4,7 @@ document.write('<div id="nav">');
 document.write('<a href="index.html">Intro</a>');
 document.write('<a href="essays.html">Essays</a>');
 document.write('<a href="memos.html">Memos</a>');
+document.write('<a href="notes.html">Notes</a>');
 document.write('<a href="etc.html">Etc</a>');
 document.write('</div>');
 document.write('<br><br>');
