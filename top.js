f233fb567735316d23e47d7623a32647462201a9
a7o <a7o@x1.carbon>
2022-03-31 21:50:15 +1100

    commit

--- top.js
+++ top.js
@@ -1,5 +1,5 @@
-document.write('<script type="text/javascript" src="pair.js"></script>');
-document.write('<br><br>');
+// document.write('<script type="text/javascript" src="mandelbrot.js"></script>');
+// document.write('<br><br>');
 document.write('<div id="nav">');
 document.write('<a href="index.html">Intro</a>');
 document.write('<a href="essays.html">Essays</a>');
